bc6db68f888db5bca5dc470b2a6b051ffda7c492
fagner de oliveira carrena <fagnercarrena@yahoo.com.br>
2023-05-29 19:08:06 -0300

    adicionando cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const rotaLivro = require('./rotas/roteador');
-
+const cors = require("cors")
 const app = express();
 
 app.use(express.json())
+app.use(cors({origin:"*"}))
+
 
 
 app.use( rotaLivro)
